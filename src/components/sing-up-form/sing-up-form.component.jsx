27f110164b91be0011a4849acd7500d839fefc75
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2023-01-24 21:24:07 +0700

    create sing up component, the form input component, button component

---
+++ src/components/sing-up-form/sing-up-form.component.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+import "./sing-up-form.style.scss";
+import Button from "../button/button.component";
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SingUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const resertFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("password do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserDocumentFromAuth(user, { displayName });
+      resertFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email alredy in use");
+      } else {
+        console.log("user creation encountered an error", error);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sing-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sing up wit your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          inputOptions={{
+            type: "text",
+            required: true,
+            onChange: handleChange,
+            name: "displayName",
+            value: displayName,
+          }}
+        />
+
+        <FormInput
+          label="Email"
+          inputOptions={{
+            type: "email",
+            required: true,
+            onChange: handleChange,
+            name: "email",
+            value: email,
+          }}
+        />
+
+        <FormInput
+          label="Password"
+          inputOptions={{
+            type: "password",
+            required: true,
+            onChange: handleChange,
+            name: "password",
+            value: password,
+          }}
+        />
+
+        <FormInput
+          label="Confirm Password"
+          inputOptions={{
+            type: "password",
+            required: true,
+            onChange: handleChange,
+            name: "confirmPassword",
+            value: confirmPassword,
+          }}
+        />
+        <Button buttonType="google" type="submit">
+          Sing Up
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SingUpForm;
--- src/routes/sing-in/sing-in.component.jsx
+++ src/routes/sing-in/sing-in.component.jsx
@@ -1,18 +1,20 @@
-import React from "react";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SingUpForm from "../../components/sing-up-form/sing-up-form.component";
 
 const SingIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sing In Page</h1>
       <button onClick={logGoogleUser}>Sing in with Google Popup</button>
+      <SingUpForm />
     </div>
   );
 };
