f87fae74a111ec3db05a18b175a54203c1296f5b
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2023-03-27 00:38:19 +0700

    edit name button

--- src/components/sing-in-form/sign-in-form.component.tsx
+++ src/components/sing-in-form/sign-in-form.component.tsx
@@ -74,7 +74,7 @@ const SignInForm = () => {
             type='button'
             onClick={signInWithGoogle}
           >
-            Sign In With Google
+            Sign In Google
           </Button>
         </ButtonsContainer>
       </form>
